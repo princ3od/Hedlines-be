ed66874d9a7b05bdc6a2b6e59b8e2a383892fe80
Trong Duong Binh <princ3od@gmail.com>
2022-05-22 23:45:18 +0700

    feat: add news crawler - spiders: vnexpress only

--- news_crawler/main_local.py
+++ news_crawler/main_local.py
@@ -1,32 +1,32 @@
+import json
 import timeit
-from scrapy.crawler import CrawlerRunner
-from twisted.internet import reactor
+from scrapy.crawler import CrawlerProcess
 
 from scrapy.utils.project import get_project_settings
 from firestore_utils import init_firebase, get_sources
 
-from news_crawler.pipelines import NewsCrawlerPipeline
 from news_crawler.spiders.vnexpress import VnexpressSpider
+from news_crawler.pipelines import NewsCrawlerPipeline
 
 
-def main(*args, **kwargs):
+def main_local(*args, **kwargs):
     start_time = timeit.default_timer()
     print(">> Start crawling...", flush=True)
 
-    init_firebase()
-    topics = get_sources()
+    db = init_firebase()
+    sources = get_sources(db)
+    NewsCrawlerPipeline.save_spider_articles = True
 
     spiders = [
         VnexpressSpider,
     ]
 
-    crawler = CrawlerRunner(get_project_settings())
+    crawler = CrawlerProcess(get_project_settings())
     for spider in spiders:
-        crawler.crawl(spider, topics)
+        crawler.crawl(spider, sources)
 
     d = crawler.join()
-    d.addBoth(lambda _: reactor.stop())
-    reactor.run(0)
+    crawler.start()
 
     number_of_articles = 0
     for topic in NewsCrawlerPipeline.articles_by_topics:
@@ -35,3 +35,14 @@ def main(*args, **kwargs):
     elapsed_time = round(timeit.default_timer() - start_time, 4)
     print(f">> Number of articles: {number_of_articles}")
     print(f">> Time elapsed: {elapsed_time}s")
+    with open(f"articles_by_topics.json", "w", encoding="utf-8") as file:
+        json.dump(
+            NewsCrawlerPipeline.articles_by_topics,
+            file,
+            ensure_ascii=False,
+            indent=2,
+            default=str,
+        )
+
+
+main_local()
--- news_crawler/news_crawler/settings.py
+++ news_crawler/news_crawler/settings.py
@@ -6,6 +6,7 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from news_crawler.utils import get_random_agent
 
 BOT_NAME = 'news_crawler'
 
@@ -14,10 +15,10 @@ NEWSPIDER_MODULE = 'news_crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'news_crawler (+http://www.yourdomain.com)'
+USER_AGENT = get_random_agent()
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -31,7 +32,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -62,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'news_crawler.pipelines.NewsCrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'news_crawler.pipelines.NewsCrawlerPipeline': 0,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
