a15aa9c9898fab3d22f0578b616331ea627269f7
Trong Duong Binh <princ3od@gmail.com>
2022-06-22 23:10:25 +0700

    chore: update articles_fetcher

--- articles_fetcher/main.py
+++ articles_fetcher/main.py
@@ -1,4 +1,5 @@
 import json
+
 from firestore_utils import get_article
 
 from firestore_utils import init_firebase, get_topics, get_editors
@@ -8,6 +9,10 @@ topics = get_topics(db)
 editors = get_editors(db)
 
 
-def fetch_article(id, user_id=None):
+def fetch_article(request):
+    payload = request.get_data(as_text=True)
+    req = json.loads(payload)
+    id = req["article_id"]
+    user_id = req["user_id"] if "user_id" in req else None
     article = get_article(id, topics, editors, db, user_id)
     return article, 200
