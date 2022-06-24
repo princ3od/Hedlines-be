7919df94814d4b74a72ad1c14099db5b1ac15321
Trong Duong Binh <princ3od@gmail.com>
2022-06-24 12:58:51 +0700

    chore: update articles_fetcher response

--- articles_fetcher/firestore_utils.py
+++ articles_fetcher/firestore_utils.py
@@ -24,24 +24,37 @@ def init_firebase() -> Client:
 
 def get_topics(db: Client) -> dict:
     topics = db.collection("topics").document("content").get().to_dict()
+    for topic in topics:
+        topics[topic]["id"] = topic
     return topics
 
 
 def get_editors(db: Client) -> dict:
     editors = db.collection("editors").document("content").get().to_dict()
+    for editor in editors:
+        editors[editor]["id"] = editor
     return editors
 
 
 def get_article(id: str, topics, editors, db: Client, user_id=None) -> dict:
     article = db.collection("articles").document(id).get().to_dict()
+    tag_orders = {tag_id: tag["ordinal"] for tag_id, tag in article["tags"].items()}
+    tag_orders = sorted(tag_orders, key=tag_orders.get)
+    tags = [{
+        "id": tag_id,
+        "tag": article["tags"][tag_id]["tag"],
+    } for tag_id in tag_orders]
+    article["tags"] = tags
     article["date"] = int(article["date"].timestamp() * 1000)
-    article["topic"] = topics[article["topic"]]
+    topic = article["topic"]
+    article["topic"] = topics[topic]
     article["source"] = editors[article["source"]]
     has_like = "liked_by" in article
     has_share = "shared_by" in article
-    article["is_liked"] = user_id in article["liked_by"] if has_like and user_id else False
+    article["is_liked"] = user_id in article["liked_by"] if has_like else False
     article["like_count"] = len(article["liked_by"]) if has_like else 0
     article["share_count"] = len(article["shared_by"]) if has_share else 0
     article.pop("liked_by", None)
     article.pop("shared_by", None)
+    article.pop("content", None)
     return article
